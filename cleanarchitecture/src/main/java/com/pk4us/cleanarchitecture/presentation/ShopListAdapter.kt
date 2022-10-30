fdff8f08677bb5dfff5ac9e6590427bee9bc836f
PK4U_S <Stepanchuk.ei@gmail.com>
2022-10-30 15:36:23 +0200

    recyclerView - DiffUtil

--- cleanarchitecture/src/main/java/com/pk4us/cleanarchitecture/presentation/ShopListAdapter.kt
+++ cleanarchitecture/src/main/java/com/pk4us/cleanarchitecture/presentation/ShopListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.pk4us.cleanarchitecture.R
 import com.pk4us.cleanarchitecture.domain.ShopItem
@@ -15,14 +16,16 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callBack = ShopListDiffCallBack(shopList,value)
+            val diffResult = DiffUtil.calculateDiff(callBack)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("MyLog", "count = ${++count}")
+//        Log.d("MyLog", "count = ${++count}")
         val layout = when (viewType) {
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
@@ -33,6 +36,7 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        Log.d("MyLog", "onBindViewHolder = ${++count}")
         val shopItem = shopList[position]
         holder.view.setOnLongClickListener {
             onShopItemLongClickListener?.invoke(shopItem)
---
+++ cleanarchitecture/src/main/java/com/pk4us/cleanarchitecture/presentation/ShopListDiffCallBack.kt
@@ -0,0 +1,29 @@
+package com.pk4us.cleanarchitecture.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.pk4us.cleanarchitecture.domain.ShopItem
+
+class ShopListDiffCallBack(
+    private val oldList: List<ShopItem>,
+    private val newList: List<ShopItem>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+}
