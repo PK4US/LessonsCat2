f65b637a9673ce99aa7bfd5211a513ba3a5a82a5
PK4U_S <Stepanchuk.ei@gmail.com>
2022-11-05 20:08:17 +0200

    recyclerView - add layout shopitem activity

---
+++ cleanarchitecture/src/main/java/com/pk4us/cleanarchitecture/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.pk4us.cleanarchitecture.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pk4us.cleanarchitecture.R
+
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
